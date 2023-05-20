ea1345424c6c0ff754af293c7c70b466a4171159
robot0804 <111331327+robot0804@users.noreply.github.com>
2023-05-20 17:31:40 +0800

    Update ha_backup.sh

--- ha_backup.sh
+++ ha_backup.sh
@@ -1,117 +1,86 @@
 #!/bin/bash
-# 定义目标文件夹和备份路径
-SOURCE="/usr/share/hassio/homeassistant"
-DESTINATION="/home/backup/ha"
+# 开启调试模式
+set -x
+# 定义源文件夹和目标文件夹
+SOURCE="/home/backup/ha"
+DESTINATION="/usr/share/hassio/homeassistant"
 # 获取当前日期和时间
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-# 创建备份文件名
-FILENAME="backup_$DATE.tar.gz"
-
-# 检查源目录是否存在 # 添加了这一行代码
-if [ ! -d "$SOURCE" ]; then # 添加了这一行代码
-  echo "源目录不存在，退出。" # 添加了这一行代码
-  exit 1 # 添加了这一行代码
-fi # 添加了这一行代码
-
-# 检查目标目录是否存在 # 添加了这一行代码
-if [ ! -d "$DESTINATION" ]; then # 添加了这一行代码
-  echo "目标目录不存在，创建它..." # 添加了这一行代码
-  sudo mkdir -p "$DESTINATION" # 添加了这一行代码
-  if [ $? -ne 0 ]; then # 添加了这一行代码
-    echo "创建目标目录失败，退出。" # 添加了这一行代码
-    exit 1 # 添加了这一行代码
-  fi # 添加了这一行代码
-fi # 添加了这一行代码
-
-# 停止Home Assistant容器
-sudo docker stop homeassistant
-echo "已停止Home Assistant容器"
-
-# 设置超时时间和循环等待间隔
-timeout=60 # 超时时间，单位为秒
-interval=5  # 循环等待间隔，单位为秒
-
-# 计算超时时间点
-start_time=$(date +%s)
-end_time=$((start_time + timeout))
-
-# 检查Home Assistant容器状态，直到容器不再运行或达到超时时间
-while [ "$(sudo docker inspect -f '{{.State.Running}}' homeassistant)" == "true" ]; do
-  current_time=$(date +%s)
-  if [ $current_time -ge $end_time ]; then
-    echo "停止Home Assistant容器超时，无法继续备份。"
-    exit 1
-  fi
-  sleep $interval
-done
-echo "Home Assistant容器已停止"
-
-# 检测目标文件夹及其上级目录权限
-PARENT_DIR=$(dirname "$DESTINATION")
-if [ ! -w "$PARENT_DIR" ]; then
-  echo "没有写入权限，将尝试设置权限为0755..."
-  sudo chmod 0755 "$PARENT_DIR"
-  if [ $? -ne 0 ]; then
-    echo "无法设置权限，请手动确保目标文件夹的上级目录（$PARENT_DIR）具有适当的权限。"
-    exit 1
-  fi
+# 定义一个日志文件名
+LOGFILE="/home/backup/restore_$DATE.log"
+# 获取源文件夹中最新的压缩文件
+FILENAME=$(find "$SOURCE" -maxdepth 1 -name "backup*.gz" -printf "%T@ %f\n" | sort -n | tail -n 1 | cut -d' ' -f2)
+# 检查源文件是否存在
+if [[ ! -f "$SOURCE/$FILENAME" ]]; then
+  echo "源文件不存在，退出。" | tee -a "$LOGFILE"
+  exit 1 # 使用exit命令退出脚本，返回错误码1。
 fi
-echo "目标文件夹及其上级目录权限已检查"
+# 定义一个函数来检查容器的状态
+check_container_status() {
+  # 获取容器名作为参数
+  local container_name=$1
+  # 获取期望的状态作为参数
+  local expected_status=$2
+  # 设置超时时间和循环等待间隔
+  local timeout=60 # 超时时间，单位为秒
+  local interval=5 # 循环等待间隔，单位为秒
+  # 计算超时时间点
+  local start_time=$(date +%s)
+  local end_time=$((start_time + timeout))
+  # 检查容器状态，直到容器达到期望的状态或达到超时时间
+  while [[ "$(sudo docker inspect -f '{{.State.Running}}' $container_name)" != "$expected_status" ]]; do
+    local current_time=$(date +%s)
+    if [[ $current_time -ge $end_time ]]; then
+      echo "检查$container_name容器状态超时，无法继续操作。" | tee -a "$LOGFILE"
+      exit 1 # 使用exit命令退出脚本，返回错误码1。
+    fi
+    sleep $interval
+  done
+}
+# 停止Home Assistant容器，并检查容器状态是否为false（即停止）
+sudo docker stop homeassistant | tee -a "$LOGFILE"
+check_container_status homeassistant false 
+echo "Home Assistant容器已停止" | tee -a "$LOGFILE"
+# 强制删除目标目录，并检查是否删除成功
 
-# 检查目标文件夹权限并设置为0755
-if [ ! -w "$DESTINATION" ]; then
-  echo "没有写入权限，将尝试设置权限为0755..."
-  sudo chmod 0755 "$DESTINATION"
-  if [ $? -ne 0 ]; then
-    echo "无法设置权限，请手动确保目标文件夹（$DESTINATION）具有适当的权限。"
-    exit 1
-  fi
+# 检查是否有足够的权限删除目标目录，如果没有则尝试使用sudo命令或者切换到root用户。
+if [[ ! -w "$DESTINATION" ]]; then
+  echo "没有写入权限，将尝试使用sudo命令或者切换到root用户..." | tee -a "$LOGFILE"
+  sudo su root # 切换到root用户，需要输入密码。
 fi
-echo "目标文件夹权限已检查"
 
-# 使用tar命令压缩和打包目标文件夹，并设置文件权限为0755
-sudo tar -czvf "$DESTINATION/$FILENAME" -C "$SOURCE" .
-sudo chmod -R 0755 "$DESTINATION"
-echo "目标文件夹已压缩和设置权限"
-
-# 检查备份是否成功
-if [ $? -eq 0 ]; then
-  echo "备份成功，文件名为$FILENAME"
-  
-  # 保留三个最新的备份文件，删除其余的备份文件 # 修改了这段代码，使用 find 和 sort 命令来排序备份文件。
-  backup_files=$(find "$DESTINATION" -name "backup*.gz" -printf "%T@ %p\n" | sort -nr | cut -d' ' -f2)
-  count=0
-  for file in $backup_files; do 
-    count=$((count+1))
-    if [ $count -gt 3 ]; then 
-      echo "删除$file"
-      rm "$file"
-    fi 
- done
-
+sudo rm -rf "$DESTINATION" | tee -a "$LOGFILE"
+if [[ ! -d "$DESTINATION" ]]; then 
+  echo "目标目录已删除成功" | tee -a "$LOGFILE"
 else 
-  echo "备份失败，请检查错误信息。"
+  echo "目标目录仍存在，无法继续还原。" | tee -a "$LOGFILE"
+  exit 1 # 使用exit命令退出脚本，返回错误码1。
 fi
 
-# 启动Home Assistant容器 
-sudo docker start homeassistant # 添加了这一行代码来启动容器。
-echo "已启动Home Assistant容器"
+# 新建并赋权目标目录，并检查是否成功
 
-# 设置超时时间和循环等待间隔 # 添加了这段代码来设定时间范围。
-timeout=600 # 超时时间，单位为秒，这里设定为10分钟。
-interval=10 # 循环等待间隔，单位为秒，这里设定为10秒。
+sudo mkdir "$DESTINATION" | tee -a "$LOGFILE"
+sudo chmod 0755 "$DESTINATION" | tee -a "$LOGFILE"
 
-# 计算超时时间点 # 添加了这段代码来计算超时时间点。
-start_time=$(date +%s)
-end_time=$((start_time + timeout))
+if [[ ! -d "$DESTINATION" ]] || [[ ! -w "$DESTINATION" ]]; then
+  echo "目标目录不存在或没有写入权限，无法继续还原。" | tee -a "$LOGFILE"
+  exit 1 # 使用exit命令退出脚本，返回错误码1。
+else 
+  echo "目标目录已准备好" | tee -a "$LOGFILE"
+fi
 
-# 检查Home Assistant容器状态，直到容器运行或达到超时时间 # 添加了这段代码来循环检查容器状态。
-while [ "$(sudo docker inspect -f '{{.State.Running}}' homeassistant)" == "false" ]; do 
- current_time=$(date +%s)
- if [ $current_time -ge $end_time ]; then 
-   echo "启动Home Assistant容器超时，请检查错误信息。"
-   exit 1 
- fi 
- sleep $interval 
-done 
-echo "Home Assistant容器正在运行"
+# 使用tar命令解压源文件，并设置文件权限为0755，并重命名为homeassistant文件夹
+sudo tar -xzvf "$SOURCE/$FILENAME" -C "$DESTINATION" --strip-components=1 | tee -a "$LOGFILE"# 使用--strip-components选项去掉第一层目录结构。
+sudo chmod -R 0755 "$DESTINATION/homeassistant" | tee -a "$LOGFILE"# 直接设置homeassistant文件夹的权限。
+echo "源文件已解压、设置权限并重命名" | tee -a "$LOGFILE"
+# 检查还原是否成功
+if [[ $? == 0 ]]; then 
+  echo "还原成功，文件名为$FILENAME" | tee -a "$LOGFILE"
+else 
+  echo "还原失败，请检查错误信息。" | tee -a "$LOGFILE"
+fi 
+# 启动Home Assistant容器，并检查容器状态是否为true（即运行）
+sudo docker start homeassistant | tee -a "$LOGFILE"
+check_container_status homeassistant true 
+echo "Home Assistant容器正在运行" | tee -a "$LOGFILE"
+
